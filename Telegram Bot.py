56493b186dcc2016a7de14b793427302729b7929
lueplestory <52071723+lueplestory@users.noreply.github.com>
2019-06-21 19:56:07 +0900

    Add files via upload

---
+++ Telegram Bot.py
@@ -0,0 +1,21 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("구매문의 Telegram9524")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("구매방법"):
+        await message.channel.send("구매는 Telegram#9524 에게 해주세요")
+
+
+client.run("NTkxMjU5ODA4NDg1NzM2NTEx.XQuLtA.RXP-SacZiE84JmPwMgpgbfHO8zI")
